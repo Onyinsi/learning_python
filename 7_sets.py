86ec767e7c1e1cb1e556ba71d51dd16f6f84224e
OnyinsiSoss <72618136+OnyinsiSoss@users.noreply.github.com>
2023-01-25 15:20:11 +0300

    updated

--- 7_sets.py
+++ 7_sets.py
@@ -1,4 +1,7 @@
-east_african_cities ={"Nairobi","Kigali","Kampala","Dar es salaam","Kinshasa","Bujumbura","Juba"  }
+east_african_cities = {
+    "Nairobi", "Kigali", "Kampala", "Dar es salaam", "Kinshasa", "Bujumbura",
+    "Juba"
+}
 
 print(east_african_cities)
 
@@ -6,11 +9,14 @@ print(f"there are {len(east_african_cities)} cities in the set")
 
 print(type(east_african_cities))
 
-west_african_cities = {"Lagos","Accra",}
-south_african_cities = {"Cape town","Gaborone"}
+west_african_cities = {
+    "Lagos",
+    "Accra",
+}
+south_african_cities = {"Cape town", "Gaborone"}
 
 master_cities_list = {"new york"}
 master_cities_list.update(east_african_cities)
 print(master_cities_list)
 
-super_cities = {"new york","Washington"}
+super_cities = {"new york", "Washington"}
--- quiz game.py
+++ quiz game.py
@@ -9,41 +9,41 @@ score = 0
 #while True:
 Quiz = input("what does cpu stand for? ")
 if Quiz.lower() == "central processing unit":
-        print("correct")
-        score += 1
+    print("correct")
+    score += 1
 else:
-        print("incorect")
+    print("incorect")
 
 Quiz = input("what does gpu stand for? ")
 if Quiz.lower() == "graphics processing unit":
-        print("correct")
-score = 0
+    print("correct")
+    score = 0
 else:
-        print("incorect")
+    print("incorect")
 
 Quiz = input("what does ram stand for? ")
 if Quiz.lower() == "random access memory":
-        print("correct")
-        score += 1
+    print("correct")
+    score += 1
 else:
-        print("incorect")
+    print("incorect")
 
 Quiz = input("what does rom stand for? ")
 if Quiz.lower() == "read only memory":
-        print("correct")
-        score += 1
+    print("correct")
+    score += 1
 else:
-        print("incorect")
+    print("incorect")
 
 Quiz = input("""what does hd stand for? 
     a.) hellodog
     b.) hard drive
     c.) hard disk  """)
 if Quiz.lower() == "c":
-        print("correct")
-        score += 1
+    print("correct")
+    score += 1
 else:
-        print("incorect")
+    print("incorect")
 print(f"You got {str(score/5*100)} %.")
 print(f"You got {str(score)} out of 5 questions correct")
-    #continue
+#continue
